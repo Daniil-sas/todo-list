3e37298c40b1443d0a6971fee4d3954b00750aa4
Zien <daniil2015473@gmail.com>
2025-04-18 13:56:59 +0300

    start lab 6

--- src/model/task-model.js
+++ src/model/task-model.js
@@ -14,6 +14,18 @@ export default class TasksModel {
         return this.#boardtasks.filter(f => f.status === status)[0];
     }
 
+    getTaskById(taskId) {
+        for (const listTask of this.#boardtasks) {
+            const taskById = listTask.tasks.filter(t => t.id === taskId)[0];
+
+            if (taskById) {
+                const currStatus = listTask.status;
+                
+                return [ currStatus, taskById ];
+            }
+        }
+    }
+
     addTask(title) {
         const newTask = {
             id: generateID(),
@@ -36,6 +48,16 @@ export default class TasksModel {
         this._notifyObservers();
     }
 
+    removeTaskFromStatus(task, status) {
+        const listTaskOfStatus = this.getTasksByStatus(status);
+
+        const indexTask = listTaskOfStatus.tasks.indexOf(task);
+
+        if (indexTask > -1) {
+            listTaskOfStatus.tasks.splice(indexTask, 1);
+        }
+    }
+
     removeBasketTask() {
         const basketTasks = this.getTasksByStatus(Status.BASKET);
 
@@ -46,6 +68,17 @@ export default class TasksModel {
 
     addObserver(observer) {
         this.#observers.push(observer);
+    }
+
+    updateTaskStatus(taskId, newStatus) {
+        const [oldStatus, task] = this.getTaskById(taskId);
+
+        if (task && oldStatus != newStatus) {
+            this.getTasksByStatus(newStatus).tasks.push(task);
+            this.removeTaskFromStatus(task, oldStatus);
+
+            this._notifyObservers();
+        }
     }
 
     removeObserver(observer) {
--- src/view/task-list-component.js
+++ src/view/task-list-component.js
@@ -5,20 +5,43 @@ function createTasksListTemplate(label, status) {
     return (
         `<div class="display-tasks ${status}">
           <h3>${label}</h3>
-          <ul class="task-container">
+          <ul>
           </ul>
         </div>`
     );
 }
 
 export default class TasksListComponent extends AbstractComponent {
-    constructor(status) {
+    #droppedTaskId = null;
+
+    constructor(status, onTaskDrop) {
         super();
         this.status = status;
+        this.#setDropHandler(onTaskDrop);
     }
 
     get template() {
         const label = StatusLabel[this.status];
         return createTasksListTemplate(label, this.status);
+    }
+
+    #setDropHandler(onTaskDrop) {
+        const container = this.element;
+
+        container.addEventListener('dragover', (evt) => {
+            evt.preventDefault();
+        });
+
+        container.addEventListener('dragenter', (evt) => {
+            evt.preventDefault();
+            this.#droppedTaskId = evt.target.id;
+        });
+
+        container.addEventListener('drop', (evt) => {
+            evt.preventDefault();
+            const taskId = evt.dataTransfer.getData('text/plain');
+            console.log(taskId, this.#droppedTaskId);
+            onTaskDrop(taskId, this.status);
+        });
     }
 }
